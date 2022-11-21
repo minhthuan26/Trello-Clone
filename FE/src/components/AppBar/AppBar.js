5432f8043a828fc1bb1e16bd78e1eeef8404e812
Taj Quach <quachhungtai29@gmail.com>
2022-11-21 13:51:45 +0700

    Clean Code, fix card mi an lien

--- FE/src/components/AppBar/AppBar.js
+++ FE/src/components/AppBar/AppBar.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Container as BoostrapContainer, Button, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
-import { logOutUser } from '../../redux/apiRequest';
+import {
+  Container as BoostrapContainer,
+  Button,
+  Row,
+  Col,
+  InputGroup,
+  FormControl,
+} from 'react-bootstrap';
+import { getAllUser, logOutUser } from '../../redux/apiRequest';
 import { createAxios } from '../../createInstance';
-import { logOutSuccess } from '../../redux/authSlice';
+import { loginSuccess, logOutSuccess } from '../../redux/authSlice';
 import './AppBar.scss';
 
 function AppBar() {
@@ -15,25 +22,25 @@ function AppBar() {
   const navigate = useNavigate();
 
   const accessToken = user?.accessToken;
-  // const [newAccessToken, setNewAccessToken] = useState('');
+
   const id = user?._id;
 
   let axiosJWT = createAxios(user, dispatch, logOutSuccess);
+
+  let axiosJWTgetUsers = createAxios(user, dispatch, loginSuccess);
+
   const LogOutUser = () => {
-    // setNewAccessToken(refreshToken(accessToken));
-    // console.log(newAccessToken);
     logOutUser(dispatch, id, navigate, accessToken, axiosJWT);
-    // navigate('/login');
   };
 
-  // useEffect(() => {
-  //   if (!user) {
-  //     navigate('/login');
-  //   }
-  //   if (user?.accessToken) {
-  //     getAllUser(user?.accessToken, dispatch, axiostJWTgetUser);
-  //   }
-  // });
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+    if (user?.accessToken) {
+      getAllUser(accessToken, dispatch, axiosJWTgetUsers);
+    }
+  });
 
   return (
     <div>
@@ -43,21 +50,30 @@ function AppBar() {
             <Row>
               <Col sm={5} xs={12} className="col-no-padding">
                 <div className="app-actions">
-                  <div className="item all"><i className="fa fa-th" /></div>
-                  <div className="item home"><i className="fa fa-home" /></div>
-                  <div className="item boards"><i className="fa fa-columns" />&nbsp;&nbsp;<strong>Boards</strong></div>
+                  <div className="item all">
+                    <i className="fa fa-th" />
+                  </div>
+                  <div className="item home">
+                    <i className="fa fa-home" />
+                  </div>
+                  <div className="item boards">
+                    <i className="fa fa-columns" />
+                    &nbsp;&nbsp;<strong>Boards</strong>
+                  </div>
                   <div className="item search">
                     <InputGroup className="group-search">
                       <FormControl
                         className="input-search"
                         placeholder="Jump to..."
                       />
-                      <InputGroup.Text className="input-icon-search"><i className="fa fa-search" /></InputGroup.Text>  
+                      <InputGroup.Text className="input-icon-search">
+                        <i className="fa fa-search" />
+                      </InputGroup.Text>
                     </InputGroup>
                   </div>
                 </div>
               </Col>
-              
+
               <Col sm={2} xs={12} className="col-no-padding">
                 <div className="logo">
                   <Link to={'/'}>
@@ -69,20 +85,20 @@ function AppBar() {
 
               <Col sm={5} xs={12} className="col-no-padding">
                 <div className="user-actions">
-                  {/* eslint-disable-next-line */}
-                  <div><a className="account-name">Hi, {user.username}</a></div>
                   <div>
-                  <Button
-                    className="logout-button"
-                    size="sm"
-                    onClick={LogOutUser}
-                  >
-                    LogOut
-                  </Button>
+                    {/* eslint-disable-next-line */}
+                    <a className="account-name">Hi, {user.username}</a>
                   </div>
                   <div>
-                    {/* <img></img> */}
+                    <Button
+                      className="logout-button"
+                      size="sm"
+                      onClick={LogOutUser}
+                    >
+                      LogOut
+                    </Button>
                   </div>
+                  <div>{/* <img></img> */}</div>
                 </div>
               </Col>
             </Row>
@@ -107,28 +123,20 @@ function AppBar() {
                   {/* eslint-disable-next-line */}
                   <div className="user-actions">
                     <Link className="link" to={'/login'}>
-                    <Button
-                          className="re-button"
-                        >
-                          Login
-                        </Button>
+                      <Button className="re-button">Login</Button>
                     </Link>
                     <Link className="link" to={'/register'}>
-                    <Button
-                          className="re-button"
-                        >
-                          Register
-                        </Button>
+                      <Button className="re-button">Register</Button>
                     </Link>
                   </div>
                 </Col>
               </Row>
-            </BoostrapContainer>  
+            </BoostrapContainer>
           </nav>
         </>
       )}
     </div>
-  )
+  );
 }
 
 export default AppBar;
--- FE/src/components/BoardContent/BoardContent.js
+++ FE/src/components/BoardContent/BoardContent.js
@@ -1,212 +1,208 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { isEmpty } from 'lodash'
-import { Container, Draggable } from 'react-smooth-dnd'
-import { Container as BootstrapContainer, Row, Col, Form, Button } from 'react-bootstrap'
-import './BoardContent.scss'
-import Column from '../Column/Column'
-// import { initialData } from '../../actions/initialData'
-import { fetchBoardDetails, createNewColumn } from '../../actions/ApiCall'
-import { mapOrder } from '../../uitilities/sort'
-import { applyDrag } from '../../uitilities/dragDrop'
+import React, { useEffect, useState, useRef } from 'react';
+import { isEmpty } from 'lodash';
+import { Container, Draggable } from 'react-smooth-dnd';
+import {
+  Container as BootstrapContainer,
+  Row,
+  Col,
+  Form,
+  Button,
+} from 'react-bootstrap';
+import './BoardContent.scss';
+import Column from '../Column/Column';
+import { fetchBoardDetails, createNewColumn } from '../../actions/ApiCall';
+import { mapOrder } from '../../uitilities/sort';
+import { applyDrag } from '../../uitilities/dragDrop';
 import { useSelector } from 'react-redux';
 
 function BoardContent() {
-    const user = useSelector((state) => state.auth.login.currentUser);
+  const user = useSelector((state) => state.auth.login.currentUser);
 
-    const [board, setBoard] = useState({})
-    const [columns, setColumns] = useState([])
-    const [openNewColumnForm, setOpenNewColumnForm] = useState(false)
-    const toggleOpenNewColumnForm = () => setOpenNewColumnForm(!openNewColumnForm) //ẩn openNewColumForm
+  const [board, setBoard] = useState({});
+  const [columns, setColumns] = useState([]);
+  const [openNewColumnForm, setOpenNewColumnForm] = useState(false);
+  const toggleOpenNewColumnForm = () =>
+    setOpenNewColumnForm(!openNewColumnForm); //ẩn openNewColumForm
 
-    const newColumnInputRef = useRef(null)
+  const newColumnInputRef = useRef(null);
 
-    const [newColumnTitle, setNewColumnTitle] = useState('')
-    const onNewColumnTitleChange = (e) => setNewColumnTitle(e.target.value)
+  const [newColumnTitle, setNewColumnTitle] = useState('');
+  const onNewColumnTitleChange = (e) => setNewColumnTitle(e.target.value);
 
-    //call API get full board
-    useEffect(() => {
-      const boardId = "635e30fbb1a6972cdc26aeb9"
-      fetchBoardDetails(boardId).then(board => {
-        setBoard(board)
-        setColumns(mapOrder(board.columns, board.columnOrder, board._id))
-      })
-    }, [])
+  //call API get full board
+  useEffect(() => {
+    const boardId = '635e30fbb1a6972cdc26aeb9';
+    fetchBoardDetails(boardId).then((board) => {
+      setBoard(board);
+      setColumns(mapOrder(board.columns, board.columnOrder, board._id));
+    });
+  }, []);
 
-    //data mẫu
-    // useEffect(() => {
-    //   const boardFromDB = initialData.boards.find(board => board.id === 'board-1')
-    //   if (boardFromDB) {
-    //     setBoard(boardFromDB)
-    //     setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
-    //   }
-    // }, [])
+  //focus con trỏ vào input
 
-    //focus con trỏ vào input
-    
-    useEffect(() => {
-      if(newColumnInputRef && newColumnInputRef.current){
-        newColumnInputRef.current.focus()
-        newColumnInputRef.current.select() //bôi đen sau khi nhập vào input nhưng không nhấn add new column
-      }
-    }, [openNewColumnForm])
+  useEffect(() => {
+    if (newColumnInputRef && newColumnInputRef.current) {
+      newColumnInputRef.current.focus();
+      newColumnInputRef.current.select(); //bôi đen sau khi nhập vào input nhưng không nhấn add new column
+    }
+  }, [openNewColumnForm]);
 
-    if(isEmpty(board)) {
-      return <div className="not-found">Board not found!</div>
+  if (isEmpty(board)) {
+    return <div className="not-found">Board not found!</div>;
+  }
+
+  const onColumnDrop = (dropResult) => {
+    let newColumns = [...columns];
+    newColumns = applyDrag(newColumns, dropResult);
+
+    let newBoards = { ...board };
+    newBoards.columnOrder = newColumns.map((c) => c._id);
+    newBoards.columns = newColumns;
+
+    setColumns(newColumns);
+    setBoard(newBoards);
+  };
+
+  const onCardDrop = (columnId, dropResult) => {
+    if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
+      let newColumns = [...columns];
+
+      let currentColumn = newColumns.find((c) => c._id === columnId);
+      currentColumn.cards = applyDrag(currentColumn.cards, dropResult);
+      currentColumn.cardOrder = currentColumn.cards.map((i) => i._id);
+
+      setColumns(newColumns);
+    }
+  };
+
+  const addNewColumn = () => {
+    if (!newColumnTitle) {
+      newColumnInputRef.current.focus();
+      return;
     }
 
-    const onColumnDrop = (dropResult) => {
-      let newColumns = [...columns]
-      newColumns = applyDrag(newColumns, dropResult)
+    const newColumntoAdd = {
+      boardId: board._id,
+      title: newColumnTitle.trim(),
+    };
 
-      let newBoards = { ...board }
-      newBoards.columnOrder = newColumns.map(c => c._id)
-      newBoards.columns = newColumns
+    //callAPI create column
+    createNewColumn(newColumntoAdd).then((column) => {
+      let newColumns = [...columns];
+      newColumns.push(column); //đẩy Column thêm mới vào cuối mảng chứa các Column
 
-      setColumns(newColumns)
-      setBoard(newBoards)
+      let newBoards = { ...board };
+      newBoards.columnOrder = newColumns.map((c) => c._id);
+      newBoards.columns = newColumns;
+
+      setColumns(newColumns);
+      setBoard(newBoards);
+      setNewColumnTitle('');
+      toggleOpenNewColumnForm();
+    });
+  };
+
+  const onUpdateColumnState = (newColumntoUpdate) => {
+    const columnIdtoUpdate = newColumntoUpdate._id;
+
+    let newColumns = [...columns];
+    const columnIndextoUpdate = newColumns.findIndex(
+      (i) => i._id === columnIdtoUpdate
+    );
+
+    if (newColumntoUpdate._destroy) {
+      //remove column
+      newColumns.splice(columnIndextoUpdate, 1); //xoa 1 phan tu o vi tri index trong newColumns
+    } else {
+      //update column
+      newColumns.splice(columnIndextoUpdate, 1, newColumntoUpdate); //sau khi xoa 1 phan tu o vi tri index, them lai gia tri column moi
     }
 
-    const onCardDrop = (columnId, dropResult) => {
-      if(dropResult.removedIndex !== null || dropResult.addedIndex !== null){
-        let newColumns = [...columns]
+    //sau khi xoa, sua column thi update lai board
+    let newBoards = { ...board };
+    newBoards.columnOrder = newColumns.map((c) => c._id);
+    newBoards.columns = newColumns;
 
-        let currentColumn = newColumns.find(c => c._id === columnId)
-        currentColumn.cards = applyDrag(currentColumn.cards, dropResult)
-        currentColumn.cardOrder = currentColumn.cards.map(i => i._id)
+    setColumns(newColumns);
+    setBoard(newBoards);
+  };
 
-        setColumns(newColumns)
-      }
-    }
+  return (
+    <div>
+      {user ? (
+        <div className="board-content">
+          <Container
+            orientation="horizontal"
+            onDrop={onColumnDrop}
+            getChildPayload={(index) => columns[index]}
+            dragHandleSelector=".column-drag-handle"
+            dropPlaceholder={{
+              animationDuration: 150,
+              showOnTop: true,
+              className: 'column-drop-preview',
+            }}
+          >
+            {columns.map((column, index) => (
+              <Draggable key={index}>
+                <Column
+                  column={column}
+                  onCardDrop={onCardDrop}
+                  onUpdateColumnState={onUpdateColumnState}
+                />
+              </Draggable>
+            ))}
+          </Container>
 
-    const addNewColumn = () => {
-      if(!newColumnTitle) {
-        newColumnInputRef.current.focus()
-        return
-      }
-
-      const newColumntoAdd = {
-        // id: Math.random().toString(36).substring(2, 5), 
-        boardId: board._id,
-        title: newColumnTitle.trim()
-        // cardOrder: [],
-        // cards: []
-      }
-
-      // let newColumns = [...columns]
-      //   newColumns.push(newColumntoAdd) //đẩy Column thêm mới vào cuối mảng chứa các Column
-
-      //   let newBoards = { ...board }
-      //   newBoards.columnOrder = newColumns.map(c => c._id)
-      //   newBoards.columns = newColumns
-
-      //   setColumns(newColumns)
-      //   setBoard(newBoards)
-      //   setNewColumnTitle('')
-      //   toggleOpenNewColumnForm()
-
-      //callAPI create column
-      createNewColumn(newColumntoAdd).then(column => {
-        let newColumns = [...columns]
-        newColumns.push(column) //đẩy Column thêm mới vào cuối mảng chứa các Column
-
-        let newBoards = { ...board }
-        newBoards.columnOrder = newColumns.map(c => c._id)
-        newBoards.columns = newColumns
-
-        setColumns(newColumns)
-        setBoard(newBoards)
-        setNewColumnTitle('')
-        toggleOpenNewColumnForm()
-      })
-    }
-
-    const onUpdateColumnState = (newColumntoUpdate) => {
-      const columnIdtoUpdate = newColumntoUpdate._id
-
-      let newColumns = [...columns]
-      const columnIndextoUpdate = newColumns.findIndex(i => i._id === columnIdtoUpdate)
-
-      if(newColumntoUpdate._destroy) {
-        //remove column
-        newColumns.splice(columnIndextoUpdate, 1) //xoa 1 phan tu o vi tri index trong newColumns
-      }else {
-        //update column
-        newColumns.splice(columnIndextoUpdate, 1, newColumntoUpdate) //sau khi xoa 1 phan tu o vi tri index, them lai gia tri column moi
-      }
-
-      //sau khi xoa, sua column thi update lai board
-      let newBoards = { ...board }
-      newBoards.columnOrder = newColumns.map(c => c._id)
-      newBoards.columns = newColumns
-
-      setColumns(newColumns)
-      setBoard(newBoards)
-    }
-
-    return(
-      <div>
-        {user ? (
-          <div className="board-content">
-            <Container
-              orientation="horizontal"
-              onDrop={onColumnDrop}
-              getChildPayload={index => columns[index]}
-              dragHandleSelector=".column-drag-handle"
-              dropPlaceholder={{
-                animationDuration: 150,
-                showOnTop: true,
-                className: 'column-drop-preview'
-              }}
-            >
-              {columns.map((column, index) => (
-                <Draggable key={index}>
-                  <Column 
-                    column={column} 
-                    onCardDrop={onCardDrop} 
-                    onUpdateColumnState={onUpdateColumnState}
-                  />
-                </Draggable>
-              ))} 
-            </Container>
-            
-            <BootstrapContainer className="bootstrap-container">
-              {!openNewColumnForm &&
-                <Row>
-                <Col className="add-new-column" onClick={toggleOpenNewColumnForm}>
-                  <i className="fa fa-plus icon" />  Add another column
+          <BootstrapContainer className="bootstrap-container">
+            {!openNewColumnForm && (
+              <Row>
+                <Col
+                  className="add-new-column"
+                  onClick={toggleOpenNewColumnForm}
+                >
+                  <i className="fa fa-plus icon" /> Add another column
                 </Col>
-                </Row>
-              }
+              </Row>
+            )}
 
-              {openNewColumnForm &&
-                <Row>
-                  <Col className="enter-new-column">
-                    <Form.Control 
-                      size="sm" 
-                      type="text" 
-                      placeholder="Enter column title..."
-                      className="input-enter-new-column"
-                      ref={newColumnInputRef} //trỏ đến effect focus trỏ chuột vào input
-                      value={newColumnTitle}
-                      onChange={onNewColumnTitleChange}
-                      onKeyDown={event => (event.key === 'Enter') && addNewColumn()}
-                    />
-                    <Button variant="success" size="sm" onClick={addNewColumn}>Add Column</Button>
-                    <span className="cancel-icon" onClick={toggleOpenNewColumnForm}> 
-                      <i className="fa fa-trash icon"></i>
-                    </span>
-                  </Col>
-                </Row>
-              }
-            </BootstrapContainer>
+            {openNewColumnForm && (
+              <Row>
+                <Col className="enter-new-column">
+                  <Form.Control
+                    size="sm"
+                    type="text"
+                    placeholder="Enter column title..."
+                    className="input-enter-new-column"
+                    ref={newColumnInputRef} //trỏ đến effect focus trỏ chuột vào input
+                    value={newColumnTitle}
+                    onChange={onNewColumnTitleChange}
+                    onKeyDown={(event) =>
+                      event.key === 'Enter' && addNewColumn()
+                    }
+                  />
+                  <Button variant="success" size="sm" onClick={addNewColumn}>
+                    Add Column
+                  </Button>
+                  <span
+                    className="cancel-icon"
+                    onClick={toggleOpenNewColumnForm}
+                  >
+                    <i className="fa fa-trash icon"></i>
+                  </span>
+                </Col>
+              </Row>
+            )}
+          </BootstrapContainer>
+        </div>
+      ) : (
+        <>
+          <div>
+            <h3>Please login to use this web</h3>
           </div>
-        ) : (
-          <>
-          <div><h3>Please login to use this web</h3></div>
-          </>
-        )}
-      </div>
-    )
+        </>
+      )}
+    </div>
+  );
 }
 
-export default BoardContent
+export default BoardContent;
