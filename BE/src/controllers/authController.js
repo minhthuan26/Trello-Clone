80dc730d322bd29fa7d4861d6dc30a7f353fd32e
minhthuan <minhthuan26042001@gmail.com>
2022-11-25 23:07:20 +0700

    update syntax

--- BE/src/controllers/authController.js
+++ BE/src/controllers/authController.js
@@ -168,7 +168,7 @@ class AuthController {
             await User.findByIdAndUpdate(
                 currentUser._id,
                 {
-                    active: true,
+                    active: !currentUser.active,
                 },
                 {
                     new: true,
